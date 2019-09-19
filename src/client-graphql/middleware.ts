36f140c1882a9b40d5ed808db06381da0e734bbf
Jonas Kello <jonas.kello@gmail.com>
2019-09-19 12:44:20 +0200

    Middleware per marker

--- src/client-graphql/middleware.ts
+++ src/client-graphql/middleware.ts
@@ -6,30 +6,79 @@ import { GraphQLError } from "graphql";
 import { createSchema } from "./schema";
 import { GetFilesDir, GetBaseUrl, createContext } from "./context";
 import { RootValue } from "./resolvers";
+import { readJsonFile } from "./read-files";
+import { buildRootFileName, RootFile } from "../file-types";
 
 export function createClientGraphQLMiddleware(
   getFilesDir: GetFilesDir,
   getBaseUrl: GetBaseUrl,
   prefix?: string
 ): Koa.Middleware {
+  const middlewarePerMarker: { [marker: string]: Koa.Middleware } = {};
   const router = new Router({ prefix });
 
-  router.all(
-    "/",
-    graphqlHTTP(async (_request, _repsonse, ctx) => ({
-      schema: await createSchema(ctx, getFilesDir),
-      graphiql: true,
-      rootValue: {} as RootValue,
-      context: createContext(ctx, getFilesDir, getBaseUrl),
-      formatError: (error: GraphQLError) => {
-        console.log("Error occured in GraphQL:");
-        console.log(error);
-        console.log("The original error was:");
-        console.log(error.originalError);
-        return error;
-      },
-    }))
-  );
+  router.all("/", async (ctx, next) => {
+    ctx.body = "No marker specified.";
+    ctx.status = 404;
+    return next();
+  });
+
+  router.all("/:marker", async (ctx, next) => {
+    const { marker } = ctx.params;
+    const rootFileContent = await readJsonFile<RootFile>(getFilesDir(ctx), buildRootFileName());
+    const m = rootFileContent.data.markers[marker];
+    if (!m) {
+      ctx.body = `Marker ${marker} not found.`;
+      ctx.status = 404;
+      return next();
+    }
+    let markerMiddleware = middlewarePerMarker[marker];
+    if (!markerMiddleware) {
+      console.log("Creating middleware");
+      markerMiddleware = graphqlHTTP(async (_request, _repsonse, ctx) => ({
+        schema: await createSchema(ctx, getFilesDir),
+        graphiql: true,
+        rootValue: {} as RootValue,
+        context: createContext(ctx, getFilesDir, getBaseUrl),
+        formatError: (error: GraphQLError) => {
+          console.log("Error occured in GraphQL:");
+          console.log(error);
+          console.log("The original error was:");
+          console.log(error.originalError);
+          return error;
+        },
+      }));
+      middlewarePerMarker[marker] = markerMiddleware;
+    }
+    return markerMiddleware(ctx, next);
+  });
 
   return compose([router.routes(), router.allowedMethods()]);
 }
+
+// export function createClientGraphQLMiddleware(
+//   getFilesDir: GetFilesDir,
+//   getBaseUrl: GetBaseUrl,
+//   prefix?: string
+// ): Koa.Middleware {
+//   const router = new Router({ prefix });
+
+//   router.all(
+//     "/",
+//     graphqlHTTP(async (_request, _repsonse, ctx) => ({
+//       schema: await createSchema(ctx, getFilesDir),
+//       graphiql: true,
+//       rootValue: {} as RootValue,
+//       context: createContext(ctx, getFilesDir, getBaseUrl),
+//       formatError: (error: GraphQLError) => {
+//         console.log("Error occured in GraphQL:");
+//         console.log(error);
+//         console.log("The original error was:");
+//         console.log(error.originalError);
+//         return error;
+//       },
+//     }))
+//   );
+
+//   return compose([router.routes(), router.allowedMethods()]);
+// }
