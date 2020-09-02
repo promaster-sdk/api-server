1fa6da8878648736edd450668fdd621ca4e11f1c
Jonathan Mourtada <jonathan.mourtada@divid.se>
2020-09-02 13:09:37 +0200

    Fix database from regex capture group

--- src/context-parsing.ts
+++ src/context-parsing.ts
@@ -4,8 +4,12 @@ import { getHeaderIgnoreCase } from "./headers";
 
 export function getDatabaseId(ctx: Koa.Context, fallbackTenantId: boolean): string {
   const tenantId = getHeaderIgnoreCase(ctx.headers, "X-Promaster-SelectedTenantId");
-  let databaseId = ctx.params.database_id;
-  if (!databaseId && fallbackTenantId) {
+
+  // First check if database_id is specified in koa name parameters.
+  // Second check if regex is used with koa router. If so it should be the first capture group
+  // If fallbackTenantId is true check the header for tenant_id and use it as database_id for backwards compability
+  let databaseId = (ctx.params.database_id || ctx.params[0] || ("" as string)).replace(/\//g, "");
+  if (!databaseId && fallbackTenantId && tenantId) {
     databaseId = tenantId;
   }
 
