8c6180ba79f1f767a8a998620753fa19d161f15e
Jonas Kello <jonaskello@users.noreply.github.com>
2020-12-11 10:40:12 +0100

    Fix blank column names (#12)
    * Make unique names safe
    
    * Skip blank column names
    
    * Filter out columns with empty name
    
    * Update changelog

--- src/client-graphql/modules/default.ts
+++ src/client-graphql/modules/default.ts
@@ -15,18 +15,20 @@ export async function createModuleType(
 ): Promise<GraphQLObjectType> {
   const fields: GraphQLFieldConfigMap<unknown, unknown, unknown> = {};
   for (const [n, v] of Object.entries(tableByName)) {
-    const tableFieldName = toSafeName(n);
-    const tableRowType = new GraphQLObjectType({
-      name: getUniqueTypeName(tableFieldName, usedTypeNames),
-      fields: buildTableRowTypeFields(v.columns),
-    });
-    fields[tableFieldName] = {
-      type: new GraphQLNonNull(GraphQLList(new GraphQLNonNull(tableRowType))),
-      description: v.description,
-      resolve: (parent: ModuleFieldResolverParent, _args: {}, ctx: Context, info: GraphQLResolveInfo) => {
-        return resolveTableRows(parent.module, info.fieldName, parent.productFileName, ctx.loaders);
-      },
-    };
+    if (n !== "") {
+      const tableFieldName = toSafeName(n);
+      const tableRowType = new GraphQLObjectType({
+        name: getUniqueTypeName(tableFieldName, usedTypeNames),
+        fields: buildTableRowTypeFields(v.columns),
+      });
+      fields[tableFieldName] = {
+        type: new GraphQLNonNull(GraphQLList(new GraphQLNonNull(tableRowType))),
+        description: v.description,
+        resolve: (parent: ModuleFieldResolverParent, _args: {}, ctx: Context, info: GraphQLResolveInfo) => {
+          return resolveTableRows(parent.module, info.fieldName, parent.productFileName, ctx.loaders);
+        },
+      };
+    }
   }
   return new GraphQLObjectType({ name: getUniqueTypeName(`Module_${moduleFieldName}`, usedTypeNames), fields });
 }
